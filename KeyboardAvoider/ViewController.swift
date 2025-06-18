27276872302329f9e79563ac3becc590d8819df7
Craig Hockenberry <craig.hockenberry@gmail.com>
2025-06-18 11:18:09 -0700

    Experimenting with a workaround:
    The keyboard layout guide's constant can be used to add an offset that's derived from the keyboard change notification. While possible, it's brittle and will look janky as it happens independently from the guide.

--- KeyboardAvoider/ViewController.swift
+++ KeyboardAvoider/ViewController.swift
@@ -14,36 +14,89 @@ class ViewController: UIViewController {
 	@IBOutlet var accessoryBarButtonItem: UIBarButtonItem!
 	@IBOutlet var responderBarButtonItem: UIBarButtonItem!
 	@IBOutlet var notificationOffsetConstraint: NSLayoutConstraint!
+	@IBOutlet var keyboardLayoutGuideConstraint: NSLayoutConstraint!
+	@IBOutlet var attachedView: UIView!
 	
-	var constraintsInitialized: Bool = false
+	var layoutInitialized: Bool = false
+	var keyboardOffset: CGFloat = 0
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+
+		var dummyText = "START\n\n"
+		for _ in 0..<10 {
+			dummyText += """
+	Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.\n\n
+	"""
+		}
+		dummyText += "END"
+		textView.text = dummyText
+		
+		/*
+		 /// Defaults to @c YES. When the keyboard is offscreen, the layout guide is tied to the bottomAnchor of the view's safeAreaLayoutGuide. Set this to @c NO to instead have the guide use the bottomAnchor of the view.
+		 @property (nonatomic, readwrite) BOOL usesBottomSafeArea API_AVAILABLE(ios(17.0));
+
+		 /// Defaults to 0.0. When a user scrolls to dismiss the keyboard (see @c UIScrollViewKeyboardDismissMode), the gesture waits to start the dismiss until it intersects with the keyboard. This adds padding above the keyboard to start the dismiss earlier. Negative values will be treated as 0.
+		 @property (nonatomic, readwrite) CGFloat keyboardDismissPadding API_AVAILABLE(ios(17.0));
+		 */
+		
+		self.view.keyboardLayoutGuide.usesBottomSafeArea = true
+		self.view.keyboardLayoutGuide.keyboardDismissPadding = attachedViewHeight
+		
+		textView.contentInsetAdjustmentBehavior = .scrollableAxes
 		
 		// NOTE: When the inputAccessoryView is assigned, the "Accessory" button is green, otherwise it's red.
 		textView.inputAccessoryView = self.customInputAccessoryView
 		accessoryBarButtonItem.tintColor = .systemGreen
 		
+		textView.textContainerInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+		
 		NotificationCenter.default.addObserver(self, selector: #selector(handleWillShowOrHide), name: UIResponder.keyboardWillShowNotification, object: nil)
 		NotificationCenter.default.addObserver(self, selector: #selector(handleWillShowOrHide), name: UIResponder.keyboardWillHideNotification, object: nil)
 	}
 
+	override func viewSafeAreaInsetsDidChange() {
+		super.viewSafeAreaInsetsDidChange()
+		
+		print("viewSafeAreaInsetsDidChange: safeAreaInsets = \(view.safeAreaInsets)")
+		textView.flashScrollIndicators()
+	}
+	
 	override func viewWillTransition(to size: CGSize, with coordinator: any UIViewControllerTransitionCoordinator) {
 		super.viewWillTransition(to: size, with: coordinator)
 		
 		coordinator.animate(alongsideTransition: nil) { [weak self] _ in
+			print("viewWillTransition: size = \(size), bottom = \(self?.view.safeAreaInsets.bottom ?? 0)")
 			self?.notificationOffsetConstraint.constant = self?.view.safeAreaInsets.bottom ?? 0
 			self?.view.layoutIfNeeded()
 		}
 	}
 
+	let attachedViewHeight: CGFloat = 44.0 // laziness
+	
 	override func viewDidLayoutSubviews() {
 		super.viewDidLayoutSubviews()
 		
-		if !constraintsInitialized {
+		if !layoutInitialized {
 			notificationOffsetConstraint.constant = view.safeAreaInsets.bottom
-			constraintsInitialized = true
+			
+			//var textViewInset: CGFloat = 0 //textView.adjustedContentInset.bottom
+
+#if false
+			// contentInsets: Never
+			textView.contentOffset.y = -view.safeAreaInsets.top
+			textView.contentInset.top = view.safeAreaInsets.top
+			textView.contentInset.bottom = view.safeAreaInsets.bottom + attachedViewHeight
+			textView.verticalScrollIndicatorInsets.bottom = attachedViewHeight
+#else
+			// contentInsets: Scrollable Axes
+			textView.contentInset.bottom = attachedViewHeight
+			textView.verticalScrollIndicatorInsets.bottom = attachedViewHeight
+#endif
+			print("viewDidLayoutSubviews: textViewInset = \(textView.contentInset.bottom), scrollIndicatorInset = \(textView.verticalScrollIndicatorInsets.bottom)")
+
+			layoutInitialized = true
 		}
 	}
 	
@@ -91,7 +144,7 @@ class ViewController: UIViewController {
 		// use view coordinates
 		let convertedKeyboardFrameEnd = keyboardFrameEnd
 #endif
-		print("keyboardFrameEnd = \(keyboardFrameEnd), convertedKeyboardFrameEnd = \(convertedKeyboardFrameEnd)")
+		print("handleWillShowOrHide: keyboardFrameEnd = \(keyboardFrameEnd), convertedKeyboardFrameEnd = \(convertedKeyboardFrameEnd)")
 		if (convertedKeyboardFrameEnd.minX < 0) {
 			// NOTE: A full width keyboard can return a minimum X value that is off the left edge of the screen.
 			// This was my first clue that we are getting view coordinates here, because that negative offset is
@@ -99,14 +152,76 @@ class ViewController: UIViewController {
 			print("Bad clams.")
 		}
 		
-		var bottomOffset: CGFloat = view.safeAreaInsets.bottom
-		let viewIntersection = view.bounds.intersection(convertedKeyboardFrameEnd)
+		var isUsingScreenCoordinates = false
+		var intersectingView = false
+		var isKeyboardShowing = viewIntersection.size.height > 0
 		
+
+		keyboardOffset = view.safeAreaInsets.bottom
+		let viewIntersection = view.bounds.intersection(convertedKeyboardFrameEnd)
+		var keyboardLayoutOffset: CGFloat = attachedViewHeight // actually the height of the accessory, but we don't know that
+		
+#if false
+		// contentInsets: Never
+		var textViewInset: CGFloat = view.safeAreaInsets.bottom + attachedViewHeight
+		var scrollIndicatorInset: CGFloat = attachedViewHeight
+
 		if !viewIntersection.isEmpty {
 			bottomOffset = viewIntersection.size.height
+			textViewInset = viewIntersection.size.height + attachedViewHeight
 		}
+#else
+		// contentInsets: Scrollable Axes
+		var textViewInset: CGFloat = attachedViewHeight
+		var scrollIndicatorInset: CGFloat = attachedViewHeight
+
+		if !viewIntersection.isEmpty {
+			intersectingView = true
+			
+			keyboardOffset = viewIntersection.size.height
+			textViewInset = keyboardOffset + attachedViewHeight - view.safeAreaInsets.bottom
+			scrollIndicatorInset = keyboardOffset + attachedViewHeight - view.safeAreaInsets.bottom
+		}
+#endif
+
+		print("handleWillShowOrHide: keyboardOffset = \(keyboardOffset), textViewInset = \(textViewInset), scrollIndicatorInset = \(scrollIndicatorInset)")
+
+
 		
-		notificationOffsetConstraint.constant = bottomOffset
+		textView.contentInset.bottom = textViewInset
+		textView.verticalScrollIndicatorInsets.bottom = scrollIndicatorInset
+		textView.flashScrollIndicators()
+
+#if false
+		/* keyboardLayoutGuideConstraint.firstItem?.layoutFrame =
+		Optional<CGRect>
+		  ▿ some : (0.0, 819.0, 820.0, 313.0)
+			▿ origin : (0.0, 819.0)
+			  - x : 0.0
+			  - y : 819.0
+			▿ size : (820.0, 313.0)
+			  - width : 820.0
+			  - height : 313.0
+		*/
+		if #available(iOS 26.0, *) {
+			if UIDevice.current.userInterfaceIdiom == .pad {
+				keyboardLayoutGuideConstraint.constant = keyboardLayoutOffset
+			}
+			else {
+				keyboardLayoutGuideConstraint.constant = 0
+			}
+		}
+		else {
+			if UIDevice.current.userInterfaceIdiom == .pad {
+				keyboardLayoutGuideConstraint.constant = keyboardLayoutOffset
+			}
+			else {
+				keyboardLayoutGuideConstraint.constant = 0
+			}
+		}
+#endif
+		
+		notificationOffsetConstraint.constant = keyboardOffset
 	}
 }
 
